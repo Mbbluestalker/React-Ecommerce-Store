40051e9761f6f877d767323d0461cd0c4581eeaf
Akorede Olawole <akoredeolawole@gmail.com>
2023-02-07 11:44:32 +0100

    created all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/cart" component={Cart}></Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
