aa2542953c6a31f8a7b0670e1dae0fc1967e095c
Akorede Olawole <akoredeolawole@gmail.com>
2023-02-12 07:54:12 +0100

    setup redux toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer from "./slices/productsSlice";
 
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
